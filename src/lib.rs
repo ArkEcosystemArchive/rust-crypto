7d60217168c0ca7a9bcd6476dc919400e6536188
supaiku <supaiku@protonmail.com>
2018-08-28 18:49:02 +0200

    feat: message signing, verify

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 extern crate secp256k1;
 extern crate sha2;
 extern crate failure;
+#[macro_use]
+extern crate serde_derive;
 extern crate serde_json;
+extern crate serde;
 #[macro_use]
 extern crate lazy_static;
 extern crate chrono;
