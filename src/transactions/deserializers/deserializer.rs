a04c9d78232597d16b451bf593aabfe042268823
supaiku <supaiku@protonmail.com>
2018-08-29 00:27:24 +0200

    feat: deserialize vote

--- src/transactions/deserializers/deserializer.rs
+++ src/transactions/deserializers/deserializer.rs
@@ -2,20 +2,23 @@ use hex;
 use transactions::transaction::Transaction;
 use enums::types::Types;
 use byteorder::{LittleEndian, ReadBytesExt};
+use bitcoin::util::base58;
+use std::io::Cursor;
 use std::io::prelude::*;
 
 pub fn deserialize(serialized: &str) -> Transaction {
     let decoded = hex::decode(serialized).unwrap();
-    let mut bytes = decoded.as_slice();
+    let mut bytes = Cursor::new(decoded.as_slice());
     let mut transaction = Transaction::default();
 
     let asset_offset = deserialize_header(&mut bytes, &mut transaction);
+    deserialize_type(&mut bytes, &mut transaction, asset_offset);
 
     transaction
 }
 
-fn deserialize_header(mut bytes: &[u8], transaction: &mut Transaction) -> u8 {
-    bytes.read_u8().unwrap();
+fn deserialize_header(bytes: &mut Cursor<&[u8]>, transaction: &mut Transaction) -> u8 {
+    transaction.header = bytes.read_u8().unwrap();
     transaction.version = bytes.read_u8().unwrap();
     transaction.network = bytes.read_u8().unwrap();
     transaction.type_id = bytes.read_u8().unwrap().into();
@@ -37,10 +40,11 @@ fn deserialize_header(mut bytes: &[u8], transaction: &mut Transaction) -> u8 {
     50 * 2 + vendor_field_length * 2
 }
 
-fn deserialize_type(mut bytes: &[u8], transaction: &mut Transaction) {
+fn deserialize_type(bytes: &mut Cursor<&[u8]>, mut transaction: &mut Transaction, asset_offset: u8) {
 
-    match transaction.type_id {
-        Types::Transfer => (),
+    let type_id = transaction.type_id.clone();
+    match type_id {
+        Types::Transfer => deserialize_vote(bytes, &mut transaction),
         Types::SecondSignatureRegistration => (),
         Types::DelegateRegistration => (),
         Types::Vote => (),
@@ -51,4 +55,13 @@ fn deserialize_type(mut bytes: &[u8], transaction: &mut Transaction) {
         Types::DelegateResignation => ()
     }
 
+}
+
+fn deserialize_vote(bytes: &mut Cursor<&[u8]>, transaction: &mut Transaction) {
+    transaction.amount = bytes.read_u64::<LittleEndian>().unwrap();
+    transaction.expiration = bytes.read_u32::<LittleEndian>().unwrap();
+
+    let mut recipient_id_buf = [0; 21];
+    bytes.read(&mut recipient_id_buf).unwrap();
+    transaction.recipient_id = base58::check_encode_slice(&recipient_id_buf);
 }
