16406c8e0161c19164fba14014bc31deb28c2f5d
supaiku <supaiku@protonmail.com>
2018-08-30 01:40:54 +0200

    test: multi_signature_registration deserialization

--- tests/transactions/deserializer/mod.rs
+++ tests/transactions/deserializer/mod.rs
@@ -2,3 +2,4 @@ pub mod delegate_registration;
 pub mod second_signature_registration;
 pub mod transfer_test;
 pub mod vote_test;
+pub mod multi_signature_registration_test;
---
+++ tests/transactions/deserializer/multi_signature_registration_test.rs
@@ -0,0 +1,50 @@
+use arkecosystem_crypto::configuration::network;
+use arkecosystem_crypto::enums::types::Types;
+use arkecosystem_crypto::networks::Network;
+use arkecosystem_crypto::transactions::deserializer;
+use *;
+
+#[test]
+fn test_signed_with_a_passphrase() {
+    let fixture = json_transaction("multi_signature_registration", "passphrase");
+    let serialized = &fixture["serialized"].as_str().unwrap();
+    let transaction = deserializer::deserialize(serialized);
+
+    network::set(Network::Devnet);
+
+    assert_eq!(transaction.version, 1);
+    assert_eq!(transaction.network, 23);
+    assert_eq!(transaction.type_id, Types::MultiSignatureRegistration);
+    assert_eq!(
+        transaction.timestamp,
+        fixture["data"]["timestamp"].as_u64().unwrap() as u32
+    );
+    assert_eq!(
+        transaction.sender_public_key,
+        fixture["data"]["senderPublicKey"].as_str().unwrap()
+    );
+    assert_eq!(
+        transaction.fee,
+        fixture["data"]["fee"].as_u64().unwrap() as u64
+    );
+    assert_eq!(
+        transaction.amount,
+        fixture["data"]["amount"].as_u64().unwrap() as u64
+    );
+    assert_eq!(
+        transaction.signature,
+        fixture["data"]["signature"].as_str().unwrap()
+    );
+    assert_eq!(
+        transaction.sign_signature,
+        fixture["data"]["signSignature"].as_str().unwrap()
+    );
+    assert_eq!(
+        transaction.signatures,
+        serde_json::from_value::<Vec<String>>(fixture["data"]["signatures"].clone()).unwrap()
+    );
+    assert_eq!(transaction.id, fixture["data"]["id"].as_str().unwrap());
+
+    let asset = fixture["data"]["asset"].clone();
+    assert_eq!(transaction.asset, serde_json::from_value(asset).unwrap());
+}
