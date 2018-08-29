dcec2adf77a54ba3b21c89c18b8b45cc06efb31f
supaiku <supaiku@protonmail.com>
2018-08-29 12:56:51 +0200

    feat: deserialize multi_signature_registration

--- src/transactions/deserializers/deserializer.rs
+++ src/transactions/deserializers/deserializer.rs
@@ -21,7 +21,7 @@ pub fn deserialize(serialized: &str) -> Transaction {
     transaction
 }
 
-fn deserialize_header(bytes: &mut Cursor<&[u8]>, transaction: &mut Transaction) -> u8 {
+fn deserialize_header(bytes: &mut Cursor<&[u8]>, transaction: &mut Transaction) -> usize {
     transaction.header = bytes.read_u8().unwrap();
     transaction.version = bytes.read_u8().unwrap();
     transaction.network = bytes.read_u8().unwrap();
@@ -41,14 +41,14 @@ fn deserialize_header(bytes: &mut Cursor<&[u8]>, transaction: &mut Transaction) 
         transaction.vendor_field_hex = hex::encode(&vendor_field_buf);
     }
 
-    50 * 2 + vendor_field_length * 2
+    (50 * 2 + vendor_field_length * 2) as usize
 }
 
 fn deserialize_type(
     bytes: &mut Cursor<&[u8]>,
     mut transaction: &mut Transaction,
     serialized: &str,
-    mut asset_offset: &mut u8,
+    mut asset_offset: &mut usize,
 ) {
     let type_id = transaction.type_id.clone();
     match type_id {
@@ -66,7 +66,9 @@ fn deserialize_type(
             &mut asset_offset,
         ),
         Types::Vote => deserialize_vote(bytes, &mut transaction, serialized, &mut asset_offset),
-        Types::MultiSignatureRegistration => (),
+        Types::MultiSignatureRegistration => {
+            deserialize_multi_signature_registration(bytes, &mut transaction, &mut asset_offset)
+        }
         Types::Ipfs => (),
         Types::TimelockTransfer => (),
         Types::MultiPayment => (),
@@ -87,15 +89,12 @@ fn deserialize_second_signature_registration(
     bytes: &mut Cursor<&[u8]>,
     transaction: &mut Transaction,
     serialized: &str,
-    asset_offset: &mut u8,
+    asset_offset: &mut usize,
 ) {
     transaction.amount = bytes.read_u64::<LittleEndian>().unwrap();
     transaction.expiration = bytes.read_u32::<LittleEndian>().unwrap();
-    transaction.asset.signature.public_key = serialized
-        .chars()
-        .skip(*asset_offset as usize)
-        .take(66)
-        .collect();
+    transaction.asset.signature.public_key =
+        serialized.chars().skip(*asset_offset).take(66).collect();
 
     *asset_offset += 66;
 }
@@ -104,13 +103,13 @@ fn deserialize_delegate_registration(
     bytes: &mut Cursor<&[u8]>,
     transaction: &mut Transaction,
     serialized: &str,
-    asset_offset: &mut u8,
+    asset_offset: &mut usize,
 ) {
-    let username_length = bytes.read_u8().unwrap();
+    let username_length = bytes.read_u8().unwrap() as usize;
     let username: String = serialized
         .chars()
-        .skip((*asset_offset as usize) + 2)
-        .take((username_length as usize) * 2)
+        .skip(*asset_offset + 2)
+        .take(username_length * 2)
         .collect();
 
     transaction.asset.delegate.username = utils::str_from_hex(&username).unwrap();
@@ -122,9 +121,9 @@ fn deserialize_vote(
     bytes: &mut Cursor<&[u8]>,
     transaction: &mut Transaction,
     serialized: &str,
-    asset_offset: &mut u8,
+    asset_offset: &mut usize,
 ) {
-    let vote_length = bytes.read_u8().unwrap();
+    let vote_length = bytes.read_u8().unwrap() as usize;
 
     *asset_offset += 2;
 
@@ -132,15 +131,11 @@ fn deserialize_vote(
         let index_start = *asset_offset + (i * 2 * 34);
         let index_end = 2 * 34 - 2;
 
-        let vote_type: String = serialized
-            .chars()
-            .skip((index_start as usize) + 1)
-            .take(1)
-            .collect();
+        let vote_type: String = serialized.chars().skip(index_start + 1).take(1).collect();
 
         let mut vote: String = serialized
             .chars()
-            .skip((index_start as usize) + 2)
+            .skip(index_start + 2)
             .take(index_end)
             .collect();
 
@@ -155,4 +150,30 @@ fn deserialize_vote(
     }
 
     *asset_offset += 2 + (vote_length * 34 * 2);
+}
+
+fn deserialize_multi_signature_registration(
+    bytes: &mut Cursor<&[u8]>,
+    transaction: &mut Transaction,
+    asset_offset: &mut usize,
+) {
+    let min = bytes.read_u8().unwrap();
+    let number_of_signatures = bytes.read_u8().unwrap() as usize;
+    let lifetime = bytes.read_u8().unwrap();
+
+    for _ in 0..number_of_signatures {
+        let mut public_key_buf = [0; 33];
+        bytes.read(&mut public_key_buf).unwrap();
+
+        transaction
+            .asset
+            .multisignature
+            .keysgroup
+            .push(hex::encode(public_key_buf.to_vec()));
+    }
+
+    transaction.asset.multisignature.min = min;
+    transaction.asset.multisignature.lifetime = lifetime;
+
+    *asset_offset += 6 + number_of_signatures * 66;
 }
