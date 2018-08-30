5c31fcde841229efbea83a0a97d0ddaccefe9b1f
supaiku <supaiku@protonmail.com>
2018-08-30 03:22:44 +0200

    test: multi_signature_registration serialization

--- tests/transactions/serializer/mod.rs
+++ tests/transactions/serializer/mod.rs
@@ -1,2 +1,3 @@
 pub mod transfer_test;
 pub mod second_signature_registration_test;
+pub mod multi_signature_registration_test;
---
+++ tests/transactions/serializer/multi_signature_registration_test.rs
@@ -0,0 +1,16 @@
+use arkecosystem_crypto::configuration::network;
+use arkecosystem_crypto::networks::Network;
+use arkecosystem_crypto::transactions::{deserializer, serializer};
+use *;
+
+#[test]
+fn test_signed_with_a_passphrase() {
+    network::set(Network::Devnet);
+
+    let fixture = json_transaction("multi_signature_registration", "passphrase");
+    let serialized = fixture["serialized"].as_str().unwrap();
+    let transaction = deserializer::deserialize(&serialized);
+
+    let actual = serializer::serialize(&transaction);
+    assert_eq!(actual, serialized);
+}
